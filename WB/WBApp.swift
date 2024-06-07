e2522b33061c784955ede3987a060898e07bfde1
Aleksandra Nikiforova <alexnikxx@gmail.com>
2024-06-08 00:29:57 +0200

    Initial Commit

---
+++ WB/WBApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBApp.swift
+//  WB
+//
+//  Created by Aleksandra Nikiforova on 08/06/24.
+//
+
+import SwiftUI
+
+@main
+struct WBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
